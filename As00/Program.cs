20381787a7dc7e03aabd8db24dfa43e08cc52092
Alexander Hjelmgaard <hjel@itu.dk>
2021-09-07 11:50:16 +0200

    Created solution file and added two projects.

---
+++ As00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace As00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
